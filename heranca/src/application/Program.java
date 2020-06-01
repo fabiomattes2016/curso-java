7b606861150c313f069ef7dc03635101d85214a0
Fábio Mattes <fabiomattes2007@gmail.com>
2020-06-01 15:38:48 -0300

    Classe abstrata

--- heranca/src/application/Program.java
+++ heranca/src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -7,51 +11,31 @@ import entities.SavingsAccount;
 public class Program {
 
 	public static void main(String[] args) {
-		Account acc = new Account(1001, "Alex", 0.0);
-		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		Locale.setDefault(Locale.US);
 		
-		// UPCASTING
-		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		List<Account> list = new ArrayList<>();
 		
-		// DOWNCASTING
-		BusinessAccount acc4 = (BusinessAccount)acc2;
-		acc4.loan(100.0);
+		list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingsAccount(1003, "Bob", 300.0, 0.01));
+		list.add(new BusinessAccount(1004, "Ana", 500.0, 500.0));
 		
-		//BusinessAccount acc5 = (BusinessAccount)acc3; // gera erro
-		if (acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
-			acc5.loan(200.0);
-			//System.out.println("Loan!");
+		double sum = 0.0;
+		
+		for (Account acc : list) {
+			sum += acc.getBalance();
 		}
 		
-		if (acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount) acc3;
-			acc5.updateBalance();
-			//System.out.println("Update!");
+		System.out.printf("Total balance: %.2f%n", sum);
+		System.out.println("-----------------------------------------");
+		
+		for (Account acc : list) {
+			acc.deposit(10.0);
 		}
 		
-		Account acc6 = new Account(1005, "Fabio", 1000.0);
-		acc6.withdraw(200.0);
-		//System.out.println(acc6.getBalance());
-		
-		Account acc7 = new SavingsAccount(1006, "Teste", 1000.0, 0.01);
-		acc7.withdraw(200.0);
-		//System.out.println(acc7.getBalance());
-		
-		Account acc8 = new BusinessAccount(1007, "Teste", 1000.0, 500.0);
-		acc8.withdraw(200.0);
-		//System.out.println(acc8.getBalance());
-		
-		Account x = new Account(1020, "Alex", 1000.0);
-		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
-		
-		x.withdraw(50.0);
-		y.withdraw(50.0);
-		
-		System.out.println("C/C: " + x.getBalance());
-		System.out.println("C/P: " + y.getBalance());
+		for (Account acc : list) {
+			System.out.printf("Upated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
 	}
 
 }
