f78d027d0a977fe5293b2269fee93bfb6d869942
Fábio Mattes <fabiomattes2007@gmail.com>
2020-06-02 15:09:59 -0300

    pilha

--- trycatch/src/application/Program.java
+++ trycatch/src/application/Program.java
@@ -6,6 +6,21 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		method1();
+		System.out.println("End of program");
+	}
+	
+	public static void method1() {
+		System.out.println("*** METHOD 1 START ***");
+		
+		method2();
+		
+		System.out.println("*** METHOD 1 END ***");
+	}
+	
+	public static void method2() {
+		System.out.println("*** METHOD 2 START ***");
+		
 		Scanner sc = new Scanner(System.in);
 		
 		try {
@@ -14,9 +29,13 @@ public class Program {
 			System.out.println(vect[position]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position");
+			e.printStackTrace();
+			sc.next();
 		} catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
+		
+		System.out.println("*** METHOD 2 END ***");
 		
 		sc.close();
 	}
